8eb9f5a239f6cd46ccddb99e5a53c19737eebcf4
Jon Knapp <jon.knapp@rightscale.com>
2013-11-05 16:36:17 -0800

    recipes now echo date to tmp file

--- cookbooks/api_has_no_dependencies/recipes/default.rb
+++ cookbooks/api_has_no_dependencies/recipes/default.rb
@@ -1,5 +1,6 @@
 bash "do nothing" do
   code <<-EOC
     echo "running has_no_dependencies::default"
+    date > /tmp/default.txt
   EOC
 end
--- cookbooks/api_has_no_dependencies/recipes/lame.rb
+++ cookbooks/api_has_no_dependencies/recipes/lame.rb
@@ -1,5 +1,6 @@
 bash "do nothing" do
   code <<-EOC
     echo "running has_no_dependencies::lame"
+    date > /tmp/lame.txt
   EOC
 end
