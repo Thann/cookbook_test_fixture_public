1ad415323185a046e5ed0d575afb84c56f9c541e
Jon Knapp <jon.knapp@rightscale.com>
2013-11-20 12:06:36 -0800

    updated default and nameless metadata to work on ruby 1.8

--- cookbooks/ui_has_default_recipe/metadata.rb
+++ cookbooks/ui_has_default_recipe/metadata.rb
@@ -6,20 +6,20 @@ version "1.0.0"
 recipe "ui_has_default_recipe::default", "Do nothing. With style1."
 
 attribute 'ui_has_default_recipe/one',
-	display_name: 'ui_has_default_recipe/one',
-	description: 'an attribute that applies to all recipes'
+	:display_name => 'ui_has_default_recipe/one',
+	:description => 'an attribute that applies to all recipes'
 
 attribute 'ui_has_default_recipe/two',
-	display_name: 'ui_has_default_recipe::default/two',
-	description: 'an attribute that applies to the ::default recipe',
-	recipes: [ 'ui_has_default_recipe::default' ]
+	:display_name => 'ui_has_default_recipe::default/two',
+	:description => 'an attribute that applies to the ::default recipe',
+	:recipes => [ 'ui_has_default_recipe::default' ]
 
 attribute 'ui_has_default_recipe/three',
-	display_name: 'ui_has_default_recipe/three',
-	description: 'an attribute that applies to the nameless recipe',
-	recipes: [ 'ui_has_default_recipe' ]
+	:display_name => 'ui_has_default_recipe/three',
+	:description => 'an attribute that applies to the nameless recipe',
+	:recipes => [ 'ui_has_default_recipe' ]
 
 attribute 'ui_has_default_recipe/fail',
-	display_name: 'ui_has_default_recipe/fail',
-	description: 'an attribute that only applies to a non-existent recipe',
-	recipes: [ 'ui_has_default_recipe::fail' ]
+	:display_name => 'ui_has_default_recipe/fail',
+	:description => 'an attribute that only applies to a non-existent recipe',
+	:recipes => [ 'ui_has_default_recipe::fail' ]
--- cookbooks/ui_has_nameless_recipe/metadata.rb
+++ cookbooks/ui_has_nameless_recipe/metadata.rb
@@ -6,20 +6,20 @@ version "1.0.0"
 recipe "ui_has_nameless_recipe", "Do nothing. With style1."
 
 attribute 'ui_has_nameless_recipe/one',
-	display_name: 'ui_has_nameless_recipe/one',
-	description: 'an attribute that applies to all recipes'
+	:display_name => 'ui_has_nameless_recipe/one',
+	:description => 'an attribute that applies to all recipes'
 
 attribute 'ui_has_nameless_recipe/two',
-	display_name: 'ui_has_nameless_recipe::default/two',
-	description: 'an attribute that applies to the ::default recipe',
-	recipes: [ 'ui_has_nameless_recipe::default' ]
+	:display_name => 'ui_has_nameless_recipe::default/two',
+	:description => 'an attribute that applies to the ::default recipe',
+	:recipes => [ 'ui_has_nameless_recipe::default' ]
 
 attribute 'ui_has_nameless_recipe/three',
-	display_name: 'ui_has_nameless_recipe/three',
-	description: 'an attribute that applies to the nameless recipe',
-	recipes: [ 'ui_has_nameless_recipe' ]
+	:display_name => 'ui_has_nameless_recipe/three',
+	:description => 'an attribute that applies to the nameless recipe',
+	:recipes => [ 'ui_has_nameless_recipe' ]
 
 attribute 'ui_has_nameless_recipe/fail',
-	display_name: 'ui_has_nameless_recipe/fail',
-	description: 'an attribute that only applies to a non-existent recipe',
-	recipes: [ 'ui_has_nameless_recipe::fail' ]
+	:display_name => 'ui_has_nameless_recipe/fail',
+	:description => 'an attribute that only applies to a non-existent recipe',
+	:recipes => [ 'ui_has_nameless_recipe::fail' ]
